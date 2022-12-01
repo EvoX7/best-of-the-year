62f70633beab87bab3d4ca0b7e17d3f177b2e361
Marc <102824384+EvoX7@users.noreply.github.com>
2022-12-01 15:33:25 +0100

    Init

---
+++ src/main/java/org/best/italy/controller/MainController.java
@@ -0,0 +1,19 @@
+package org.best.italy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String getHome(Model model) {
+		
+		String name = "Marcello";
+		
+		model.addAttribute("name", name);
+		
+		return "home"; 
+	}
+}
